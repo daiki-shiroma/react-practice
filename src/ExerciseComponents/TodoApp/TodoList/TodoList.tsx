8e514c343bd8a6c72ad7d164e573d4233d20264f
daiki.shiroma <daiki.shiroma@medley.jp>
2024-04-15 12:32:49 +0900

    TodoListが空の時その旨を表示するようにした

--- src/ExerciseComponents/TodoApp/TodoList/TodoList.tsx
+++ src/ExerciseComponents/TodoApp/TodoList/TodoList.tsx
@@ -7,34 +7,44 @@ type TodoListProps = {
 
 export function TodoList({ todoList }: TodoListProps) {
   // TODO: フィルタリングロジックを実装してください https://github.com/Ryochike/react-practice/issues/7
+  const isTodoListExsit = todoList.length > 0;
   const filteredTodoList = todoList;
 
   return (
-    <List spacing={2} w="100%">
-      {filteredTodoList.map((todo) => {
-        return (
-          <ListItem key={todo.id}>
-            <HStack justify="space-between">
-              <Checkbox
-                isChecked={todo.completed}
-                onChange={() => {
-                  alert("実装してください");
-                }}
-              >
-                {todo.title}
-              </Checkbox>
-              <Button
-                size="xs"
-                onClick={() => {
-                  alert("実装してください");
-                }}
-              >
-                削除
-              </Button>
-            </HStack>
-          </ListItem>
-        );
-      })}
-    </List>
+    <>
+      {
+        isTodoListExsit ?
+          <List spacing={2} w="100%">
+            {filteredTodoList.map((todo) => {
+              return (
+                <ListItem key={todo.id}>
+                  <HStack justify="space-between">
+                    <Checkbox
+                      isChecked={todo.completed}
+                      onChange={() => {
+                        alert("実装してください");
+                      }}
+                    >
+                      {todo.title}
+                    </Checkbox>
+                    <Button
+                      size="xs"
+                      onClick={() => {
+                        alert("実装してください");
+                      }}
+                    >
+                      削除
+                    </Button>
+                  </HStack>
+                </ListItem>
+              );
+            })}
+          </List>
+          :
+          <p>
+            タスクがありません。
+          </p>
+      }
+    </>
   );
 }
