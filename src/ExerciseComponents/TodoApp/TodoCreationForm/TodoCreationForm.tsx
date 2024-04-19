864dda0a6b8832bc877c99aa9b0fd7e80986e5ca
daiki.shiroma <daiki.shiroma@medley.jp>
2024-04-19 19:01:03 +0900

    フォームを修正した

--- src/ExerciseComponents/TodoApp/TodoCreationForm/TodoCreationForm.tsx
+++ src/ExerciseComponents/TodoApp/TodoCreationForm/TodoCreationForm.tsx
@@ -37,6 +37,7 @@ export function TodoCreationForm({ createTodo }: TodoCreationFormProps) {
 
     createTodo({ title: newTaskName });
     setNewTaskName("");
+    setTitleError(undefined);
   }
 
   return (
