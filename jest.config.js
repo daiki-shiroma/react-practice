a5988a26c9f66342ed0f443d05824028838ca8cb
daiki.shiroma <daiki.shiroma@medley.jp>
2024-05-13 17:58:43 +0900

    yarn dev時に、tsconfig.jsonのjsxが変わらないようにした

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,15 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
+    // This is necessary because next.js forces { "jsx": "preserve" }, but
+    // ts-jest appears to require { "jsx": "react" }
+    "^.+\\.[jt]sx?$": [
+      "ts-jest",
+      {
+        tsconfig: {
+          jsx: "react",
+        },
+      },
+    ],
   },
 };
